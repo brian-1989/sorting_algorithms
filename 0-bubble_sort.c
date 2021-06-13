d86cd14179a357f853c8f84d5cb3276091933f6a
Brian Zapata <dzapata397@gmail.com>
2021-06-13 07:01:54 +0000

    Created the function bubble sort Commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,24 @@
+#include "sort.h"
+
+/**
+ *bubble_sort - function that sorts an array of integers in
+ *ascending order using the Bubble sort algorithm
+ * @array: the pointer at array
+ * @size: number of elements in the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i = 0, j = 0, temp;
+
+	for( ; i < size ; i++)
+	{
+		for( ; j < size - 1 ; j++)
+		{
+			if(array[j] > array[j + 1])
+				temp = array[j], array[j] = array[j + 1],
+				array[j + 1] = temp,
+				print_array(array, size);
+		}
+		j = 0;
+	}
+}
