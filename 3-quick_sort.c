cd85dfcfc3fa9336c26f5ce3a4988ddcc0c54178
Brian Zapata <dzapata397@gmail.com>
2021-06-15 15:21:26 +0000

    Organized the order of the functions Commit

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,42 +1,6 @@
 #include "sort.h"
 
 /**
- * quick_sort - function that sorts an array of integers in
- * ascending order using the Quick sort algorithm.
- * @array: the pointer at array.
- * @size: number of elements in the array.
- */
-void quick_sort(int *array, size_t size)
-{
-	int low_array = 0, high_array = 0;
-
-	high_array = size - 1;
-	if (array != NULL && size != '\0')
-		the_original_quick_sort(array, low_array, high_array, size);
-}
-
-/**
- * the_original_quick_sort - function that is implemented
- * to be able to use recursion.
- * @array: the pointer at array.
- * @low_array: first number of the array.
- * @high_array: last number of the array.
- * @size: number of elements in the array.
- */
-void the_original_quick_sort(int *array, int low_array,
-	int high_array, size_t size)
-{
-	int temp = 0;
-
-	if (low_array < high_array)
-	{
-		temp = division_array(array, low_array, high_array, size);
-		the_original_quick_sort(array, low_array, temp - 1, size);
-		the_original_quick_sort(array, temp + 1, high_array, size);
-	}
-}
-
-/**
  * division_array - function that is implemented to divide the array.
  * @array: the pointer at array.
  * @low_array: first number of the array.
@@ -66,3 +30,40 @@ int division_array(int *array, int low_array, int high_array, size_t size)
 		print_array(array, size);
 	return (i + 1);
 }
+
+/**
+ * the_original_quick_sort - function that is implemented
+ * to be able to use recursion.
+ * @array: the pointer at array.
+ * @low_array: first number of the array.
+ * @high_array: last number of the array.
+ * @size: number of elements in the array.
+ */
+void the_original_quick_sort(int *array, int low_array,
+	int high_array, size_t size)
+{
+	int temp = 0;
+
+	if (low_array < high_array)
+	{
+		temp = division_array(array, low_array, high_array, size);
+		the_original_quick_sort(array, low_array, temp - 1, size);
+		the_original_quick_sort(array, temp + 1, high_array, size);
+	}
+}
+
+/**
+ * quick_sort - function that sorts an array of integers in
+ * ascending order using the Quick sort algorithm.
+ * @array: the pointer at array.
+ * @size: number of elements in the array.
+ */
+void quick_sort(int *array, size_t size)
+{
+	int low_array = 0, high_array = 0;
+
+	high_array = size - 1;
+	if (array != NULL && size != '\0')
+		the_original_quick_sort(array, low_array, high_array, size);
+}
+
